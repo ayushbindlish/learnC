2c84c3ab791b451276b6467a7c703c0c65de1778
Ayush Bindlish <ayushbindlish@gmail.com>
2021-02-16 22:42:14 +0530

    Added helloWorld and F_Ctable

---
+++ main.c
@@ -0,0 +1,8 @@
+//gcc -Wall -Wextra -Werror -o main main.c
+#include <stdio.h> // include information about standard library
+
+int main(void) { // define a function named main that receives no argument values
+    printf("Hello World\n");
+    puts("Hello");
+    return 0;
+}
