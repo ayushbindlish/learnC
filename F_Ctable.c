5cd8ffa9e1922ad209a7a5a7c9b25527d08b3891
Ayush Bindlish <ayushbindlish@gmail.com>
2021-02-16 22:55:07 +0530

    Changed while loop to for loop in F_Ctable.c

--- F_Ctable.c
+++ F_Ctable.c
@@ -6,18 +6,9 @@
  * fahr = 0,20,...,300 */
 
 int main() {
-   float fahr, celsius;
-   int lower, upper, step;
-
-   lower = 0; // lower limit of temperature table
-   upper = 300; // upper limit of temperature table
-   step = 20; // step size
-
-   fahr = lower;
+   int fahr;
    printf("Fahr Celsius\n");
-   while(fahr<=upper) {
-       celsius = 5 * (fahr-32) / 9;
-       printf("%3.0f %6.1f\n",fahr, celsius);
-       fahr+=step;
+   for(fahr = 0; fahr<=300;fahr+=20) {
+       printf("%3.0f %6.1f\n",fahr, (5.0/ 9.0) * (fahr-32));
    }
 }
