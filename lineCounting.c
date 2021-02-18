a8a5c42bbbff5c211c537569eb8d92affd905439
Ayush Bindlish <ayushbindlish@gmail.com>
2021-02-18 22:37:01 +0530

    added lineCounting.c

---
+++ lineCounting.c
@@ -0,0 +1,18 @@
+//
+// Created by ab on 18/02/21.
+//
+#include <stdio.h>
+
+#define NAME "AYUSH"
+
+// count number of lines
+int main(void) {
+    int c ,nl;
+    nl=0;
+    while((c=getchar())!=EOF)
+        if (c=='\n')
+            ++nl;
+    printf("%d\n",nl);
+    return 0;
+
+}
